6cd7d61364b6dd8090adaae0ac10ec91eb8c96c5
Mohamed <40402787+Mohagames205@users.noreply.github.com>
2019-04-26 20:49:44 +0200

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,10 +1,10 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
+$servername = "remotemysql.com";
+$username = "qTFfXC5hAo";
+$password = getenv("password");
 
 try {
-    $handle = new PDO("mysql:host=$servername;dbname=schoolpanel", $username, $password);
+    $handle = new PDO("mysql:host=$servername;dbname=qTFfXC5hAo", $username, $password);
     // set the PDO error mode to exception
     $handle->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
     }
